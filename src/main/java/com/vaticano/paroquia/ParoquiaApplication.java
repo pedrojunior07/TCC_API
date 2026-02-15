b4c64800bc3bc9465c588a1b88e52f4927d39b4d
Pedro Junior Manjate <132139653+pedrojunior07@users.noreply.github.com>
2026-02-15 16:06:56 +0200

    First commit

---
+++ src/main/java/com/vaticano/paroquia/ParoquiaApplication.java
@@ -0,0 +1,13 @@
+package com.vaticano.paroquia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParoquiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParoquiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaticano/paroquia/ParoquiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaticano.paroquia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParoquiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
